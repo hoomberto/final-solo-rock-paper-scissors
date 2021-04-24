813fa83ca1d05f7ba7df61a0f9817a3b379fb0c6
Bobby Vasquez <shootbv@gmail.com>
2021-04-23 21:08:19 -0600

    Add functions to botz.js to check move accuracy and to compare elements between player and computer

--- botz.js
+++ botz.js
@@ -32,3 +32,30 @@ var zodiac = [
   aquarius,
   pisces
 ];
+
+function checkAccuracy(moveAccuracy) {
+  var randomToCompare = Math.floor(Math.random() * 100 + 1);
+  if (randomToCompare > moveAccuracy) {
+    return false
+  }
+  else {
+    return true
+  }
+}
+
+function compareElements(currentPlayer, opponent) {
+  if (currentPlayer.element === opponent.element) {
+    console.log(`Element Tie! ${currentPlayer.element} is the same as ${opponent.element}\n`)
+  }
+  if (currentPlayer.element === 'air' && opponent.element === 'earth' || currentPlayer.element === 'earth' && opponent.element === 'water' || currentPlayer.element === 'water' && opponent.element === 'fire' || currentPlayer.element === 'fire' && opponent.element === 'air') {
+    currentPlayer.hasElementAdvantage = true;
+    console.log(`Element win! ${currentPlayer.element} won against ${opponent.element}\n`)
+  }
+    if (currentPlayer.element === 'air' && opponent.element === 'fire' || currentPlayer.element === 'earth' && opponent.element === 'air' || currentPlayer.element === 'water' && opponent.element === 'earth' || currentPlayer.element === 'fire' && opponent.element === 'water') {
+    opponent.hasElementAdvantage = true;
+    console.log(`Element loss! ${currentPlayer.element} lost against ${opponent.element}\n`)
+  }
+    if (currentPlayer.element === 'air' && opponent.element === 'water' || currentPlayer.element === 'earth' && opponent.element === 'fire' || currentPlayer.element === 'water' && opponent.element === 'air' || currentPlayer.element === 'fire' && opponent.element === 'earth') {
+    console.log(`Element Tie! ${currentPlayer.element} tied against ${opponent.element}\n`)
+  }
+}
