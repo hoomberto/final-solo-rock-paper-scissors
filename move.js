ab0d844e4d6cbd1d7dda82aa2ddb2bdfc8b04316
Bobby Vasquez <shootbv@gmail.com>
2021-04-23 21:02:46 -0600

    Create move.js, add class Move

---
+++ move.js
@@ -0,0 +1,9 @@
+class Move {
+  constructor(name, description, damage, accuracy, type) {
+    this.name = name;
+    this.description = description;
+    this.damage = damage;
+    this.accuracy = accuracy
+    this.type = type;
+  }
+}
