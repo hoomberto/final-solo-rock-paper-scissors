5661d3d90a212efddff20065cfd908b8369f98ca
Bobby Vasquez <shootbv@gmail.com>
2021-04-23 21:02:05 -0600

    Rename sign.js to zodiac.js, add class Zodiac

---
+++ zodiac.js
@@ -0,0 +1,32 @@
+class Zodiac {
+  constructor(name, element, quality) {
+    this.hp =  100,
+    this.stats = {
+    shield: 0,
+    attack: 0,
+    defense: 0,
+    speed: 0,
+    accuracy: 1,
+  },
+    this.buffs = {
+    shield: 0,
+    attack: 0,
+    defense: 0,
+    speed: 0,
+  },
+  this.statusAilments = {
+    confused: false,
+    paralyzed: false,
+    burnt: false,
+    frozen: false
+  },
+  this.name = name,
+  this.element = element,
+  this.elementMultiplier = 1,
+  this.quality = quality,
+  this.qualityMultiplier = 1,
+  this.hasElementAdvantage = false;
+  this.hasQualityAdvantage = false;
+  this.moves = []
+  }
+}
