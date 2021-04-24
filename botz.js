d5ff3d7e8c52784daffe9acecbebe9bac950261b
Bobby Vasquez <shootbv@gmail.com>
2021-04-23 21:05:53 -0600

    Create botz.js, add variables for new zodiac instances

---
+++ botz.js
@@ -0,0 +1,21 @@
+// Fire Signs
+var aries = new Zodiac("aries", "fire", "cardinal");
+var leo = new Zodiac("leo", "fire", "fixed");
+var sagittarius = new Zodiac("sagittarius", "fire", "mutable");
+
+// Earth Signs
+var virgo = new Zodiac("virgo", "earth", "cardinal");
+var taurus = new Zodiac("taurus", "earth", "fixed");
+var capricorn = new Zodiac("capricorn", "earth", "mutable");
+
+// Air Signs
+var libra = new Zodiac("libra", "air", "cardinal");
+var aquarius = new Zodiac("aquarius", "air", "fixed");
+var gemini = new Zodiac("gemini", "air", "mutable");
+
+// Water Signs
+var cancer = new Zodiac("cancer", "water", "cardinal");
+var scorpio = new Zodiac("scorpio", "water", "fixed");
+var pisces = new Zodiac("pisces", "water", "mutable");
+
+var zodiac = [aries, taurus, gemini, cancer, leo, virgo, libra, scorpio, sagittarius, capricorn, aquarius, pisces];
